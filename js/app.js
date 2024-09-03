8ee4523015e7d118ca89e414a45d16f7d4b207af
Rajib Reza <rajibreza64@gmail.com>
2024-09-03 11:50:33 +0600

    add deposit button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
+//  modify deposit button
 document.getElementById('depositBtn').addEventListener('click',function(){
     const inputDepositNumber = document.getElementById('depositField');
     const inputDepositAddValue = inputDepositNumber.value;
-    
+    const inputDepoValueStrToNum = parseFloat(inputDepositAddValue);
+    // add deposit total 
 
+    const depositTotal = document.getElementById('depositTotal');
+    const depositTotalInnerText = depositTotal.innerText;
+    const depositTotalStToNum = parseFloat(depositTotalInnerText);
+    
+    //  add total deposit add deposit section
+
+    const currentDeposit = inputDepoValueStrToNum + depositTotalStToNum;
+
+    depositTotal.innerText = currentDeposit;
+    
+    //  emtry numnber deposti field
+
+    inputDepositNumber.value = '';
+   
+    
     
 })
