2b8c273c076ee5f34183946179c80ce51216b7b3
Rajib Reza <rajibreza64@gmail.com>
2024-09-03 19:35:33 +0600

    withdraw section

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,14 +1,37 @@
 document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField = document.getElementById('withdraw-field');
-    const withdrawValue = withdrawField.value;
-    const withdrawValueString = parseFloat(withdrawValue);
+    const withdrawFieldValue = withdrawField.value;
+    const withdrawFiValStrToNum = parseFloat(withdrawFieldValue);
+    const currentWithdraw = document.getElementById('current-withdraw');
+    const currentWithdrawInnerText = currentWithdraw.innerText;
+    const currentWithdrawStrToNum = parseFloat(currentWithdrawInnerText);
+    const totalWithdraw = withdrawFiValStrToNum + currentWithdrawStrToNum;
+    currentWithdraw.innerText = totalWithdraw;
 
-    const withdraw = document.getElementById('current-withdraw');
-    const withdrawInnerText = withdraw.innerText;
-    const withdrawString = parseFloat(withdrawInnerText);
+    //  addd total balance and witdraw
 
-    withdrawField.innerText = withdrawString;
-    console.log(typeof withdrawString);
+    const balacneNegativeToWithdraw = document.getElementById('currentBalanceElement');
+    const balanceAddInnerText = balacneNegativeToWithdraw.innerText;
+    const balacneStrToNumber = parseFloat(balanceAddInnerText);
+    const newBalanceTotal = balacneStrToNumber - currentWithdrawStrToNum;
+    balacneNegativeToWithdraw.innerText = newBalanceTotal;
+    
     withdrawField.value = '';
+
+    
     
 })
+// document.getElementById('btn-withdraw').addEventListener('click',function(){
+//     const withdrawField = document.getElementById('withdraw-field');
+//     const withdrawValue = withdrawField.value;
+//     const withdrawValueString = parseFloat(withdrawValue);
+
+//     const withdraw = document.getElementById('current-withdraw');
+//     const withdrawInnerText = withdraw.innerText;
+//     const withdrawString = parseFloat(withdrawInnerText);
+
+//     withdrawField.innerText = withdrawString;
+//     console.log(typeof withdrawString);
+//     withdrawField.value = '';
+    
+// })
